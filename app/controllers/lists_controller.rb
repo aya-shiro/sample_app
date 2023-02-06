561b54dc3e94aaca1cff46eb2c1dd7b4ac19517b
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-02-06 02:19:04 +0000

    [start-3]sample_app

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,17 @@
 class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  # 投稿内容保存アクション
+  def create
+    #引数(list_params)はprivateメソッド配下のストロングパラメータの呼び出し?
+    # DWOではフォームからの入力データ受取とあるが(list: params[:list])と同義か？
+    list = List.new(list_params)
+    list.save
+    redirect_to("/top")
+  end
+
   def index
   end
 
@@ -6,5 +19,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  # アクション制限の境界線
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
